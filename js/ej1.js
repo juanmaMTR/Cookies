33e6eb507d1deb8e09186da5d0606200caa3e4d3
juanmaMTR <jtoscanoreyes.guadalupe@alumnado.fundacionloyola.net>
2021-11-15 12:19:42 +0100

    Inicial

---
+++ js/ej1.js
@@ -0,0 +1,16 @@
+/**
+ *   Descripción principal del fichero.
+ *   Descripción secundaria.
+ *   @author Juan Manuel Toscano Reyes <jtoscanoreyes.guadalupe>
+ *   @license GPL-3.0-or-later
+ *   Ref: https://spdx.org/licenses/
+ *
+ *   Ref JSDoc: https://jsdoc.app/
+ */
+'use strict'
+console.log('asdasd');
+window.onload=iniciar
+function iniciar(){
+    console.log(document.cookie);
+    document.cookie="usuario=Juanma"
+}
