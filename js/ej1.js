5faadd85326eaa320e24670b17580a855d9d63d7
juanmaMTR <jtoscanoreyes.guadalupe@alumnado.fundacionloyola.net>
2021-11-15 12:33:26 +0100

    Cookie creada

--- js/ej1.js
+++ js/ej1.js
@@ -11,6 +11,14 @@
 console.log('asdasd');
 window.onload=iniciar
 function iniciar(){
-    console.log(document.cookie);
-    document.cookie="usuario=Juanma"
+    //Creamos una cookie para ello creamos también una fecha para que dure 30 días por ejemplo
+    let fechaCaducidad= new Date()
+    fechaCaducidad.setDate(fechaCaducidad.getDate()+30)
+    /*let fechaUTC=Date.UTC(
+        fechaCaducidad.getFullYear(),
+        fechaCaducidad.getMonth(),
+        fechaCaducidad.getDate())*/
+    document.cookie="usuario=Juanma;expires="+fechaCaducidad
+    //console.log(document.cookie);
+    //Para eliminar una cookie, se le pone en fecha -1 en vez de +30 y se le quita el nombre al usuario
 }
